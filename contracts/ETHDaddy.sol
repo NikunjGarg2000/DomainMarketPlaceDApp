a62568185959dec633560db612222e71326bf40e
Nikunj Garg <nikunjgarg2000@gmail.com>
2022-12-17 21:12:02 +0530

    NFT Contract

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract ETHDaddy {
-    string public name = "ETH Daddy";
+// importing openzeppelin contracts to use ERC732 protocol which is a standard.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721{
+
+    // developer of NFT
+    address public owner;
+
+    constructor(string memory _name, string memory _symbol) 
+        ERC721(_name, _symbol) 
+    {
+        // msg.sender giving the address of them who is calling constructor
+        owner = msg.sender;
+    }
 }
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -8,11 +8,38 @@ const tokens = (n) => {
 }
 
 describe("ETHDaddy", () => {
-  it('has a name', async() => {
+
+  let ethDaddy
+  let deployer, owner1
+
+  const NAME = 'ETH Daddy'
+  const SYMBOL = 'ETHD'
+
+  beforeEach(async () => {
+    // Signers is an object that contain wallets of users.
+    // Setup accounts
+    [deployer, owner1] = await ethers.getSigners()
+    
+    // Deploy contract
     // getting javascript version of our contract using ethers.js library
     const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
-    let ethDaddy = await ETHDaddy.deploy()
-    const result = await ethDaddy.name()
-    expect(result).to.equal('ETH Daddy')
+    ethDaddy = await ETHDaddy.deploy('ETH Daddy', 'ETHD')
+  })
+
+  describe('Deployment', () => {
+    it('has a name', async() => {
+      const result = await ethDaddy.name()
+      expect(result).to.equal(NAME)
+    })
+  
+    it('has a symbol', async() => {
+      const result = await ethDaddy.symbol()
+      expect(result).to.equal(SYMBOL)
+    })
+
+    it('Sets the owner', async() => {
+      const result = await ethDaddy.owner()
+      expect(result).to.equal(deployer.address)
+    })
   })
 })  
