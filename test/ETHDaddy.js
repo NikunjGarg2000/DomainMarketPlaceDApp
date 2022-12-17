4b15807da17b918d0badc22cb1c773864590e4e3
Nikunj Garg <nikunjgarg2000@gmail.com>
2022-12-17 20:39:59 +0530

    Smoke Test

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,18 @@
+// using chai and mocha for testing.
+// mocha comes with hardhat and chat is imported below.
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
-
-})
+  it('has a name', async() => {
+    // getting javascript version of our contract using ethers.js library
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
+    let ethDaddy = await ETHDaddy.deploy()
+    const result = await ethDaddy.name()
+    expect(result).to.equal('ETH Daddy')
+  })
+})  
